b6b323005db9a2a12e867cfd43c611aaa7831b92
Ted Herman <ted-herman@uiowa.edu>
2014-10-17 11:23:00 -0500

    Setup of SearchEngine Project

---
+++ README.md
@@ -0,0 +1,3 @@
+This is just a "getting started" Java / Eclipse 
+project that can be cloned. Really more a demo
+of a Git project than anything useful.
---
+++ src/org/uiowa/cs2820/engine/Field.java
@@ -0,0 +1,14 @@
+package org.uiowa.cs2820.engine;
+
+// this is a mock class, not the real thing
+public class Field {
+	Field(String FieldName, String FieldValue) {
+		return;
+	    }
+	String getFieldName() {
+	    return "";  
+	    }
+	String getFieldValue() {
+		return "";
+	    }
+	}
